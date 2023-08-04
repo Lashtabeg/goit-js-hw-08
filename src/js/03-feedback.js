b0a0cbfeabcba5a7dc14ce6df3aa162a9c5e3a35
Lashtabeg <tahcik@live.com>
2023-08-04 22:07:47 +0800

    fix: 3rd task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,34 @@
-const form = document.getElementsByClassName(`feedback-form`);
-const subBtn = document.getElementsByTagName(`button`);
+import throttle from 'lodash.throttle';
 
-form.addEventListener('input', onInput);
+const LOCAL_KEY = `feedback-form-state`;
 
-subBtn.addEventListener('click', handleClick);
+let formData = {};
 
-function handleClick(event) {
-  preventDefault();
+const form = document.querySelector(`.feedback-form`);
+
+form.addEventListener('input', throttle(onInputData, 500));
+form.addEventListener('submit', onFormSub);
+
+setInput();
+
+function onInputData(event) {
+  const inputName = event.target.name;
+  const textMess = event.target.value;
+  formData[inputName] = textMess;
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+  console.log(localStorage.setItem(LOCAL_KEY, JSON.stringify(formData)));
+}
+
+function setInput() {
+  const getText = JSON.parse(localStorage.getItem(LOCAL_KEY, formData));
+  if (getText) {
+    form.email.value = getText.email;
+    form.message.value = getText.message;
+  }
+}
+
+function onFormSub(ev) {
+  ev.preventDefault();
+  ev.target.reset();
+  localStorage.removeItem(LOCAL_KEY);
 }

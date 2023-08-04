7819b0435b1c40aaecb62fc3e6e4b2144fbb5454
Lashtabeg <128055885+Lashtabeg@users.noreply.github.com>
2023-08-04 02:59:37 +0000

    Deploying to gh-pages from @ Lashtabeg/goit-js-hw-08@d9b94d73461d9aafc082ea6c5e99738fac977db7 🚀

---
+++ 03-feedback.258475ae.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementsByClassName("feedback-form"),t=document.getElementsByTagName("button");e.addEventListener("input",onInput),t.addEventListener("click",(function(e){preventDefault()}))}();
+//# sourceMappingURL=03-feedback.258475ae.js.map
---
+++ 03-feedback.c9a3fde7.js
@@ -0,0 +1,2 @@
+const e=document.getElementsByClassName("feedback-form"),t=document.getElementsByTagName("button");e.addEventListener("input",onInput),t.addEventListener("click",(function(e){preventDefault()}));
+//# sourceMappingURL=03-feedback.c9a3fde7.js.map

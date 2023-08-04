d9b94d73461d9aafc082ea6c5e99738fac977db7
Lashtabeg <tahcik@live.com>
2023-08-04 10:59:02 +0800

    fix: 1st task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const ulGal = document.querySelector(`.gallery`);
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+         alt="${description}"/></a></li>`;
+  })
 
-console.log(galleryItems);
+  .join(' ');
+ulGal.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  /* options */
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,10 @@
+const form = document.getElementsByClassName(`feedback-form`);
+const subBtn = document.getElementsByTagName(`button`);
 
+form.addEventListener('input', onInput);
+
+subBtn.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  preventDefault();
+}

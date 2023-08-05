6a71d4aad2db2e38944d0a049c8d0028f531f152
Lashtabeg <tahcik@live.com>
2023-08-05 22:46:00 +0800

    FINAL!!

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,33 +1,27 @@
 import throttle from 'lodash.throttle';
 
 const LOCAL_KEY = `feedback-form-state`;
-
-let formData = {};
-
 const form = document.querySelector(`.feedback-form`);
+const getText = JSON.parse(localStorage.getItem(LOCAL_KEY));
+if (getText) {
+  form.email.value = getText.email;
+  form.message.value = getText.message;
+}
 
 form.addEventListener('input', throttle(onInputData, 500));
 form.addEventListener('submit', onFormSub);
 
-setInput();
-
-function onInputData(event) {
-  const inputName = event.target.name;
-  const textMess = event.target.value;
-  formData[inputName] = textMess;
-  localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
-}
-
-function setInput() {
-  const getText = JSON.parse(localStorage.getItem(LOCAL_KEY, formData));
-  if (getText) {
-    form.email.value = getText.email;
-    form.message.value = getText.message;
-  }
+function onInputData() {
+  const saveData = {
+    email: form.email.value,
+    message: form.message.value,
+  };
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(saveData));
 }
 
 function onFormSub(ev) {
   ev.preventDefault();
-  ev.target.reset();
+  console.log(getText);
   localStorage.removeItem(LOCAL_KEY);
+  ev.currentTarget.reset();
 }

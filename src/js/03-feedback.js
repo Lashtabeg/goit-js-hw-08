66f63c31ac7bc15ca3a3648de6a40ee469582353
Lashtabeg <tahcik@live.com>
2023-08-06 18:20:16 +0800

    FINAL!!!!!

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,32 +2,19 @@ import throttle from 'lodash.throttle';
 
 const LOCAL_KEY = `feedback-form-state`;
 const form = document.querySelector(`.feedback-form`);
-// const getText = JSON.parse(localStorage.getItem(LOCAL_KEY));
-
-// if (getText) {
-//   form.email.value = getText.email;
-//   form.message.value = getText.message;
-// }
+let saveData = {};
 
 form.addEventListener('input', throttle(onInputData, 500));
 form.addEventListener('submit', onFormSub);
 
-let saveData = {};
-
 function onInputData(e) {
   const { name, value } = e.target;
   saveData[name] = value.trim();
-
-  // const saveData = {
-  //   email: form.email.value,
-  //   message: form.message.value,
-  // };
   localStorage.setItem(LOCAL_KEY, JSON.stringify(saveData));
 }
 
 function onFormSub(ev) {
   ev.preventDefault();
-  // const setData = JSON.parse(localStorage.getItem('feedback-form-state'));
   console.log(saveData);
   saveData = {};
   localStorage.removeItem(LOCAL_KEY);
@@ -41,18 +28,10 @@ function onLoad() {
     saveData = JSON.parse(data);
     Object.entries(saveData).forEach(([key, val]) => {
       form.elements[key].value = val;
-    }); //[[key, value], [key, value]]
+    });
   } catch (error) {
     console.log(error.message);
   }
 }
 
 window.addEventListener('load', onLoad);
-
-// const user = {
-//   name: 'Poly',
-//   age: 25,
-// };
-// const key = 'age';
-// console.log(user.name);
-// console.log(user[key]);
